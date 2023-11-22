3df295161756c77ffacbf2c14fa8e7528c0b11c2
mattnearyy <90067385+mattnearyy@users.noreply.github.com>
2023-11-22 10:17:30 +0000

    Update copying.js

--- copying.js
+++ copying.js
@@ -11,20 +11,3 @@ let jsonString = JSON.stringify(jsonList);
 
 // Save to local storage
 localStorage.setItem('myStyles', jsonString);
-
-
-// IN RECEIVING APP
-
-let retrievedStylesJSON = localStorage.getItem('myStyles');
-
-let stylesList = JSON.parse(retrievedStylesJSON);
-
-stylesList.length
-
-stylesList.forEach(styleObj => {
-  // Destructure the style object to extract the needed properties.
-  const { display, type, properties, states, transitions, id, source_appname } = styleObj;
-  
-  // Call the create_new_style function with the extracted values.
-  appquery.create_new_style(display, type, properties, states, transitions, id, source_appname);
-});
